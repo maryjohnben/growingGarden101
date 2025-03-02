5676456482c32ce18b2f7190e9023c2fd6a72b31
Fitzter64 <flying_fitz@yahoo.com>
2025-03-01 22:17:44 -0600

    Update About.jsx
    Change Grow to Growing. Minor text changes

--- garden-frontend/src/pages/About.jsx
+++ garden-frontend/src/pages/About.jsx
@@ -6,18 +6,18 @@ export const About = () => {
         <>
             <div>
                 <h1 style={{ fontFamily: 'cursive' + '', fontSize: '40' +'px', marginLeft:'30px' }}>
-                    About Grow Garden 101
+                    About Growing Garden 101
                 </h1>
 
                 <p style={{ fontFamily: 'Avenir' + '', fontSize: '20' + 'px', maxWidth: "800px", marginLeft:'60px', textIndent:'30px'}}>
-                    Welcome to Grow Garden 101, your ultimate gardening companion! Whether you're a 
-                    seasoned green thumb or just starting out with your very first seedling, Grow Garden 101 
+                    Welcome to Growing Garden 101, your ultimate gardening companion! Whether you're a 
+                    seasoned "green thumb" or just starting out with your very first seedling, Growing Garden 101 
                     is here to support you every step of the way. Our mission is simple: to make gardening 
                     accessible, enjoyable, and successful for everyone, regardless of experience level.
                 </p>
 
                 <p style={{ fontFamily: 'Avenir' + '', fontSize: '20' + 'px', maxWidth: "800px", marginLeft:'60px' , textIndent:'30px'}}>
-                    At Grow Garden 101, we believe that gardening is more than just a hobby—it's a way to 
+                    At Growing Garden 101, we believe that gardening is more than just a hobby—it's a way to 
                     connect with nature, grow your own fresh produce, and create a sustainable lifestyle. 
                     That's why we offer a wealth of resources, expert tips, and easy-to-follow guides tailored 
                     to beginners and experienced gardeners alike. From planning your garden layout to 
@@ -27,7 +27,7 @@ export const About = () => {
                 
                 <p style={{ fontFamily: 'Avenir' + '', fontSize: '20' + 'px', maxWidth: "800px", marginLeft:'60px', textIndent:'30px'}}>
                     Join a thriving community of garden enthusiasts who share their experiences, challenges, 
-                    and triumphs. Let Grow Garden 101 inspire you to transform your outdoor space, nurture 
+                    and triumphs. Let Growing Garden 101 inspire you to transform your outdoor space, nurture 
                     your plants, and cultivate your gardening skills. Together, let's grow something amazing!
                 </p>
              </div>
